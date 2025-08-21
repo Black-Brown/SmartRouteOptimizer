da737bd378ddf2a8617f5c8bebebd1494f3d2c7b
jose colon <131679429+Darkpolar23@users.noreply.github.com>
2025-08-21 17:52:00 -0400

    jose colon -clase ruta-

---
+++ src/SmartRouteOptimizer.Api/Models/RouteDto.cs
@@ -0,0 +1,9 @@
+﻿namespace LastMileOptimizer.Api.Models;
+public class RouteDto
+{
+    public int VehicleId { get; set; }
+    public string VehicleColor { get; set; } = "#000000";
+    public List<double[]> Coordinates { get; set; } = new(); // [[lat,lng], ...]
+    public int Deliveries { get; set; }
+    public double EstimatedDistanceKm { get; set; }
+}
