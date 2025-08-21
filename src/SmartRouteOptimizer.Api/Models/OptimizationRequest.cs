cb504b105f808065ed15c74c56bd228686a8ece2
Jheinel-Brown <109928519+Black-Brown@users.noreply.github.com>
2025-08-21 17:39:11 -0400

    feat: Create ClienteDto, VehicleDto and OptimizationRequest
    add models ClienteDto, VehicleDto and OptimizationRequest.
    
    Jheinel Brown

---
+++ src/SmartRouteOptimizer.Api/Models/OptimizationRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartRouteOptimizer.Api.Models
+{
+    public class OptimizationRequest
+    {
+        public required List<ClientDto> Clients { get; set; }
+        public required List<VehicleDto> Vehicles { get; set; }
+        public int TimeLimitSeconds { get; set; } = 30;
+    }
+}
